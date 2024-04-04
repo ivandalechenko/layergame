03766a9fcb781a7ec103d93634ac1a9e43337b28
unknown <ivandalechenko@gmail.com>
2024-04-04 16:08:42 +0300

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: resolve(__dirname, 'index.html'),
+                ourMission: resolve(__dirname, 'ourMission.html'),
                 technology: resolve(__dirname, 'technology.html'),
             }
         }
